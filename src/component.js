18c7ea38ea612fedbdb4862da9ab19966571618b
sunmar25 <tungldinfo@gmail.com>
2021-07-11 22:26:36 +0700

    nav middle done

--- src/component.js
+++ src/component.js
@@ -1,21 +1,19 @@
-const bottomFixedHeight = document.getElementsByClassName('fixed-bottom');
-const setRightHeight = document.getElementsByClassName('right');
-
 const switchChecked = document.getElementById('switch-checked');
 const switchBtn = document.getElementById('switch-btn');
 
 //take width for content right
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
+    const bottomFixedHeight = document.getElementsByClassName('fixed-bottom');
+    const setRightHeight = document.getElementsByClassName('right');
     let zx = bottomFixedHeight[0].offsetHeight;
     setRightHeight[0].style.height = String(window.innerHeight - zx) + "px";
 })
 //switch button change text
-switchBtn.addEventListener('click',()=>{
-    //when click, state checked is false
-    if(!switchChecked.checked){
-        switchBtn.setAttribute('data-content','Nghe gần đây')
-    }else{
-        switchBtn.setAttribute('data-content','Danh sách phát')
+switchBtn.addEventListener('click', () => {
+    if (switchChecked.checked) {
+        switchBtn.setAttribute('data-content', 'Danh sách phát')
+    } else {
+        switchBtn.setAttribute('data-content', 'Nghe gần đây')
     }
 });
 
