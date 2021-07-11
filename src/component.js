26e4f535fcde1b4236e48cde944c590e425de8d5
sunmar25 <tungldinfo@gmail.com>
2021-07-11 13:26:52 +0700

    content right done

--- src/component.js
+++ src/component.js
@@ -1,8 +1,21 @@
 const bottomFixedHeight = document.getElementsByClassName('fixed-bottom');
 const setRightHeight = document.getElementsByClassName('right');
 
+const switchChecked = document.getElementById('switch-checked');
+const switchBtn = document.getElementById('switch-btn');
 
+//take width for content right
 window.addEventListener('load', function() {
     let zx = bottomFixedHeight[0].offsetHeight;
     setRightHeight[0].style.height = String(window.innerHeight - zx) + "px";
 })
+//switch button change text
+switchBtn.addEventListener('click',()=>{
+    //when click, state checked is false
+    if(!switchChecked.checked){
+        switchBtn.setAttribute('data-content','Nghe gần đây')
+    }else{
+        switchBtn.setAttribute('data-content','Danh sách phát')
+    }
+});
+
